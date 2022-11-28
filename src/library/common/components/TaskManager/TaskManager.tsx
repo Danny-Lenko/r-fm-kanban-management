f79c24adda445f8c67f57b2661a63d6c09bb8f34
Danny-Lenko <devdanny.14@gmail.com>
2022-11-28 15:42:32 +0200

    got stuck with the checkbox background color

--- src/library/common/components/TaskManager/TaskManager.tsx
+++ src/library/common/components/TaskManager/TaskManager.tsx
@@ -1,23 +1,51 @@
 import Overlay from '../Overlay/Overlay';
 import Box from '@mui/material/Box'
-// import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { useAppSelector } from '../../hooks/hooks';
 import DotsMenu from '../DotsMenu/DotsMenu';
 import { useTheme } from '@mui/material/styles';
 
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { useFormik } from 'formik';
+// import * as yup from 'yup';
+// import Button from '@mui/material/Button';
+// import TextField from '@mui/material/TextField';
+
+// const validationSchema = yup.object({
+//    email: yup
+//       .string()
+//       .email('Enter a valid email')
+//       .required('Email is required'),
+//    password: yup
+//       .string()
+//       .min(8, 'Password should be of minimum 8 characters length')
+//       .required('Password is required'),
+// });
 
 const TaskManager = () => {
    const task = useAppSelector(state => state.data.managedTask)
    const theme = useTheme()
    console.log(task)
 
+   const formik = useFormik({
+      initialValues: {
+         email: 'foobar@example.com',
+         password: 'foobar',
+      },
+      validationSchema: null,
+      onSubmit: (values) => {
+         alert(JSON.stringify(values, null, 2));
+      },
+   });
+
    const taskManageStyles = {
       zIndex: 11000,
-      p: {xs: 3, sm: 4},
+      p: { xs: 3, sm: 4 },
       borderRadius: '8px',
-      width: {sm: '480px'},
+      width: { sm: '480px' },
       '& .heading': {
          display: 'flex',
          justifyContent: 'space-between',
@@ -33,10 +61,35 @@ const TaskManager = () => {
          mb: 3
       },
       '& .subtasks-heading': {
-         color: theme.palette.mode === 'light' ? 'greyCustom.200' : 'common.white'
+         color: theme.palette.mode === 'light' ? 'greyCustom.200' : 'common.white',
+         mb: 2
       }
    }
-   
+
+   const assembleCheckboxStyles = (subtask:any) => ({
+      backgroundColor: theme.palette.background.default,
+      mt: 1,
+      borderRadius: 1,
+      '& .MuiCheckbox-root': {
+         '& .MuiSvgIcon-root': {
+            color: theme.palette.divider,
+            backgroundColor: theme.palette.background.paper,
+         },
+         '&.Mui-checked': {
+            '& .MuiSvgIcon-root': {
+               color: 'primary.main'
+            }
+         }
+      },
+      '& .MuiTypography-root': {
+         mb: 0,
+         fontSize: 12/16 + 'rem',
+         fontWeight: 700,
+         color: !subtask.isCompleted ? theme.palette.text.primary : 'greyCustom.200',
+         textDecoration: subtask.isCompleted && 'line-through'
+      }
+   })
+
    return (
       <Overlay>
          <Paper elevation={0} sx={taskManageStyles}>
@@ -45,15 +98,59 @@ const TaskManager = () => {
                <DotsMenu />
             </Box>
             <Typography variant='body1'>{task.description}</Typography>
-            <Typography 
-               className='subtasks-heading' 
+            <Typography
+               className='subtasks-heading'
                variant='body2'
-               >
-                  Subtasks ({task.completedSubtasks} of {task.subtasks.length})
-               </Typography>
+            >
+               Subtasks ({task.completedSubtasks} of {task.subtasks.length})
+            </Typography>
+
+            <div>
+               <form onSubmit={formik.handleSubmit}>
+
+                  <FormGroup>
+                     {
+                        task.subtasks.map(sub => 
+                           <FormControlLabel 
+                              sx={ assembleCheckboxStyles(sub) }
+                              control={<Checkbox defaultChecked={sub.isCompleted} />} 
+                              label={sub.title} 
+                           />
+                        )
+                     }
+                  </FormGroup>
+
+
+                  {/* <TextField
+                     fullWidth
+                     id="email"
+                     name="email"
+                     label="Email"
+                     value={formik.values.email}
+                     onChange={formik.handleChange}
+                     error={formik.touched.email && Boolean(formik.errors.email)}
+                     helperText={formik.touched.email && formik.errors.email}
+                  />
+                  <TextField
+                     fullWidth
+                     id="password"
+                     name="password"
+                     label="Password"
+                     type="password"
+                     value={formik.values.password}
+                     onChange={formik.handleChange}
+                     error={formik.touched.password && Boolean(formik.errors.password)}
+                     helperText={formik.touched.password && formik.errors.password}
+                  /> */}
+                  {/* <Button color="primary" variant="contained" fullWidth type="submit">
+                        Submit
+                     </Button> */}
+               </form>
+            </div>
+
          </Paper>
       </Overlay>
    );
 }
- 
+
 export default TaskManager;
