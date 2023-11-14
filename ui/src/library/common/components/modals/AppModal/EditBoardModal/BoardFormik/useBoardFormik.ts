d3d380a69faaf3bee9d91248d719bf78aa9dc953
Valerii Danylenko <devdanny.14@gmail.com>
2023-11-14 14:15:06 +0200

    fixed edit board formik errors; need to add create new column

--- ui/src/library/common/components/modals/AppModal/EditBoardModal/BoardFormik/useBoardFormik.ts
+++ ui/src/library/common/components/modals/AppModal/EditBoardModal/BoardFormik/useBoardFormik.ts
@@ -1,7 +1,13 @@
-import { useAppSelector, useAppDispatch } from '../../../../../hooks';
-
 import { BoardValues, saveBoardChanges, createBoard } from '.';
 import {
+   useAppSelector,
+   useAppDispatch,
+   useGetQuery,
+   getQueryNames,
+} from '../../../../../hooks';
+import { IBoard } from '../../../../../../interfaces';
+import {
+   selectActiveBoardId,
    selectActiveBoardInfo,
    selectActiveColumnId,
    selectActiveTask,
@@ -10,41 +16,56 @@ import {
 } from '../../../../../../../main/store';
 
 export const useBoardFormik = () => {
-   const boards = useAppSelector(selectBoards);
-   const { activeBoard, activeBoardId } = useAppSelector(selectActiveBoardInfo);
-   const { columns } = activeBoard;
+   // const boards = useAppSelector(selectBoards);
+   // const { activeBoard, activeBoardId } = useAppSelector(selectActiveBoardInfo);
+   // const { columns } = activeBoard;
 
-   const activeColumnId = useAppSelector(selectActiveColumnId);
-   const activeTask = useAppSelector(selectActiveTask);
+   // const activeColumnId = useAppSelector(selectActiveColumnId);
+   // const activeTask = useAppSelector(selectActiveTask);
 
-   const boardIsExisting = useAppSelector(selectBoardIsExisting);
+   // const boardIsExisting = useAppSelector(selectBoardIsExisting);
+
+   const boardId = useAppSelector(selectActiveBoardId);
+
+   const boardDetails = getQueryNames.boardDetails;
+   const { isLoading, error, data } = useGetQuery<IBoard>(
+      boardDetails,
+      boardId,
+   );
 
    const dispatch = useAppDispatch();
 
-   const initialValues = boardIsExisting
+   const initialValues = data
       ? {
-           name: activeBoard.name,
-           columns: activeBoard.columns.map((col) => col.name),
+           name: data.name,
+           columns: data.columns.map((col) => col.name),
         }
       : {
            name: '',
            columns: ['', ''],
         };
 
-   const submissionParams = {
-      columns,
-      boards,
-      activeBoard,
-      activeBoardId,
-      dispatch,
-      activeTask,
-      activeColumnId,
-   };
+   // const initialValues = {
+   //    name: '',
+   //    columns: ['', ''],
+   // };
 
-   const submit = (values: BoardValues) => {};
-   // boardIsExisting
-   //    ? saveBoardChanges({ values, ...submissionParams })
-   //    : createBoard({ values, ...submissionParams });
+   // const submissionParams = {
+   //    columns,
+   //    boards,
+   //    activeBoard,
+   //    activeBoardId,
+   //    dispatch,
+   //    activeTask,
+   //    activeColumnId,
+   // };
+
+   const submit = (values: BoardValues) =>
+      initialValues.name
+         ? console.log('edit board')
+         : console.log('create board');
+   // ? saveBoardChanges({ values, ...submissionParams })
+   // : createBoard({ values, ...submissionParams });
 
    return {
       initialValues,
